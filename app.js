6d8df7ab45b35f4f4c8bd7df399806af51bb794c
Mateo <misterrattail@gmail.com>
2017-04-11 19:06:58 -0700

    added search filter

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW"]; 
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW"];
+
 $('#searchBox').hide(); 
+
 var loaded_first_time = false; 
 
+//calls  createEachUser function
 function getUserHTML(users){ 
   var users = users.join()
   var url = 'https://api.twitch.tv/kraken/users?login='+users+'&api_version=5'
@@ -19,11 +22,13 @@ function getUserHTML(users){
   });
 };
 
+//loads page for the first time for all
 if(!loaded_first_time){
  getUserHTML(channels)
  loaded_first_time = true; 
 }; 
 
+//creates html to user appends to page
 function createEachUser(data){
 
   var getUserHTML = function(){
@@ -31,9 +36,9 @@ function createEachUser(data){
     _.each(data, function(userArr){
       _.each(userArr, function(userObj){
         if(userObj.logo){
-          $('#users').append('<div class="non_live_channel"><img id="user_img" src='+userObj.logo+'><a target="_blank" href=https://www.twitch.tv/'+userObj.name+'><h1>'+userObj.display_name+'</h1></a><div class="live_status_circle" id='+userObj.name+'></div></div>');
+          $('#users').append('<li class="non_live_channel"><img id="user_img" src='+userObj.logo+'><a target="_blank" href=https://www.twitch.tv/'+userObj.name+'><h1>'+userObj.display_name+'</h1></a><div class="live_status_circle" id='+userObj.name+'></div></li>');
             } else {
-          $('#users').append('<div class="non_live_channel"><img id="user_img" src=bolt.png><a target="_blank" href=https://www.twitch.tv/'+userObj.name+'><h1>'+userObj.display_name+'</h1></a><div class="live_status_circle" id='+userObj.name+'></div></div>');
+          $('#users').append('<li class="non_live_channel"><img id="user_img" src=bolt.png><a target="_blank" href=https://www.twitch.tv/'+userObj.name+'><h1>'+userObj.display_name+'</h1></a><div class="live_status_circle" id='+userObj.name+'></div></li>');
         }
       })
     })
@@ -44,6 +49,7 @@ function createEachUser(data){
   checkLiveStream(data)
 }; 
 
+//checks if user is live
 function checkLiveStream(data){
   function checkEachId(id){ 
     var url = 'https://api.twitch.tv/kraken/streams/'+id 
@@ -72,20 +78,7 @@ function checkLiveStream(data){
   });
 };
 
-// function labelNonLive(){    
-//  var non_live_channel = document.getElementsByClassName('user');
-//  $(non_live_channel).addClass("not_live");
-//  $(non_live_channel).removeClass("user");
-//  $('.live').hide();
-//  $('.not_live').show();
-// }
-
-// function filterNotLive(data){
-//   console.log('two')
-//   $('.non_live_channel').show();
-//   $('.live').hide();
-// }
-
+//hide searchBox from other pages
 function hideSearchBox(){
   $('#search').hide();
   $('#users').css({'margin-top': '141px'}) 
@@ -150,3 +143,30 @@ $('#search').on('click', function(){
   $('#searchBox').show();
   $('#users').css({'margin-top': '0'}) 
   }); 
+
+$('#srch-term').keyup(function(event) {
+  var wholeWord =""; 
+  var wordTyped = $("#srch-term").val();
+  wholeWord += wordTyped;
+  filterDom(wholeWord);
+
+ });
+
+
+function filterDom(word){
+   
+  // Declare variables
+  var filter, ul, li, a, i;
+  filter = word.toUpperCase();
+  ul = document.getElementById("users");
+  li = ul.getElementsByTagName('li');
+  // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+    a = li[i];
+    if (a.innerText.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+      } else {
+      li[i].style.display = "none"
+    }
+  }
+};
